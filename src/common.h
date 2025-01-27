4e4080108981fdbeb88cd60857e151e4e4ce9f85
Sau P <sau@yaaama.xyz>
2025-01-27 11:57:46 +0000

    new(common.h) Basic definitions

---
+++ src/common.h
@@ -0,0 +1,23 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+typedef signed long long s64;
+
+typedef char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+typedef float f32;
+typedef double f64;
+
+
+#endif // COMMON_H_

907d7e84051ee7518d268361b4699c6209818ff3
Sau P <sau@yaaama.xyz>
2025-03-09 17:02:08 +0000

    test(lsp): Add new test for didOpen + fix old
    - Just making the tests a little cleaner.

--- test/test_lsp.c
+++ test/test_lsp.c
@@ -6,11 +6,10 @@
 #include "../src/lsp.h"
 #include "../src/pipeline.h"
 
-
 Test (test_lsp, test_initialize) {
 
     msg_t* initialize = malloc(sizeof(msg_t));
-    initialize->content =
+    char* content =
         "{\n"
         "  \"jsonrpc\": \"2.0\",\n"
         "  \"id\": 1,\n"
@@ -31,12 +30,60 @@ Test (test_lsp, test_initialize) {
         "  }\n"
         "}";
 
-    unsigned long len = strlen(initialize->content);
-    initialize->len = len;
+    initialize->content = malloc(strlen(content) + 10);
+    if (initialize->content == NULL) {
+        /* Handle allocation failure */
+        free(initialize);
+        return;
+    }
 
-    /* initialize->len = 356; */
+    strcpy(initialize->content, content);
+    initialize->len = strlen(initialize->content);
 
     pipeline_dispatcher(stdout, initialize, false);
 
+    free(initialize->content);
+    free(initialize);
+}
 
+Test (test_lsp, test_doc_DidOpen) {
+
+    msg_t* didOpen = malloc(sizeof(msg_t));
+    char* content =
+        "{\r\n"
+        "  \"jsonrpc\": \"2.0"
+        "\",\r\n"
+        "  \"id\": 3,\r\n"
+        "  \"method\": \"text"
+        "Document/didOpen\","
+        "\r\n"
+        "  \"textDocument\": "
+        "{\r\n"
+        "    \"uri\": \"/abc/"
+        "uri/to/no/where/\","
+        "\r\n"
+        "    \"languageId\": "
+        "\"markdown doc\",\r"
+        "\n"
+        "    \"version\": 900"
+        "0,\r\n"
+        "    \"text\": \"blah"
+        "blahline2and line 3!"
+        "\"\r\n"
+        "  }\r\n"
+        "}";
+
+    didOpen->content = malloc(strlen(content) + 10);
+    if (didOpen->content == NULL) {
+        free(didOpen);
+        return;
+    }
+
+    strcpy(didOpen->content, content);
+    didOpen->len = strlen(didOpen->content);
+
+    pipeline_dispatcher(stdout, didOpen, false);
+
+    free(didOpen->content);
+    free(didOpen);
 }

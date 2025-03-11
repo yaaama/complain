c2bcf3e74accf5a328ea82682f306b729f4ffa62
Sau P <sau@yaaama.xyz>
2025-03-11 10:48:40 +0000

    new: Helpful debugging macros

--- src/common.h
+++ src/common.h
@@ -23,6 +23,32 @@ typedef double f64;
 
 #define ARRAY_LENGTH(a) (sizeof((a)) / sizeof(((a)[0])))
 
+#ifdef NDEBUG
+#define COMPLAIN_TODO(message) ((void)0)
+#define COMPLAIN_UNREACHABLE(message) ((void)0)
+#else
+
+/* If stdio is not included then lets define it */
+#if !defined(_STDIO_H_) && !defined(_STDIO_H)
+int printf(const char *restrict format, ...);
+int fprintf(FILE *restrict stream, const char *restrict format, ...);
+#endif
+
+/* Define our debugging macros */
+#define COMPLAIN_TODO(message)                                             \
+    do {                                                                   \
+        fprintf(stderr, "%s:%d: TODO: %s\n", __FILE__, __LINE__, message); \
+        abort();                                                           \
+    } while (0)
+#define COMPLAIN_UNREACHABLE(message)                                   \
+    do {                                                                \
+        fprintf(stderr, "%s:%d: UNREACHABLE: %s\n", __FILE__, __LINE__, \
+                message);                                               \
+        abort();                                                        \
+    } while (0)
+
+#endif
+
 void trim_trailing_ws(char *str, u64 len);
 
 char *trim_leading_ws(char *str);
